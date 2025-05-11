de2a9456cd7edefb3bc8ebea69294b0f1286eecf
Github d0ikim <doyclub@naver.com>
2025-05-11 12:04:55 +0900

    자료형 실습 - object변수 선언, 형변환

---
+++ 01.js
@@ -0,0 +1,11 @@
+const me = {
+  name: '김도이',
+  age: 27,
+  hobby: ['뜨개질', '유튜브시청', '독서'],
+  height: 168,
+  weight: 'secret',
+  location: '금천구',
+  MBTI: 'ISTJ',
+};
+
+console.log(me);
---
+++ 02.js
@@ -0,0 +1,5 @@
+const mathScore = '77';
+const engScore = '88';
+
+let avgScore = (Number(mathScore) + Number(engScore)) / 2;
+console.log(avgScore);
