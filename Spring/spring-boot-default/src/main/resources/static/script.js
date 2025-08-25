d64e31ece27a67f07bcc8ea7fddb079541ed40e3
Doi Kim <doyclub@naver.com>
2025-08-25 13:45:05 +0900

    250825월_Spring(2-1) 정적파일
    src/main/resources/static 은 정적파일 위치
    localhost:PORT 로부터 경로를 입력하면 갖고있는 파일 보기 가능

---
+++ Spring/spring-boot-default/src/main/resources/static/script.js
@@ -0,0 +1,7 @@
+document.getElementById('mainBtn')?.addEventListener('click', () => {
+  alert('메인 페이지 버튼 클릭!');
+});
+
+document.getElementById('aboutBtn')?.addEventListener('click', () => {
+  alert('About 페이지 버튼 클릭!');
+});
