348b461c66adddc5f003cefaf9f750a359ed3ef2
Doi Kim <doyclub@naver.com>
2025-08-27 15:24:33 +0900

    250827수_SpringBoot(3-3) GET 요청_@RequestParam_@PathVariable_필수경로변수_선택경로변수_
    [GET Method]
    - Get Method URL 매핑 시 @Controller 클래스 내 에서 @GetMapping(url)을 이용
    ```
    @GetMapping(url주소)
    public String 함수이름() {
        return 템플릿파일명;
    }
    ```
    
    [query string]
    - URL query string (?key=value) 을 Controller 에서 @RequestParam 이용해 받아오기
    ```
    @GetMapping(url주소)
    public String 함수이름(@RequestParam(value="key") String key) {
        return 템플릿파일명;
    }
    ```
    - 메서드의 매개변수 key에 query string의 value 값이 전달됨
    - @RequestParam에 required=false 값을 설정하면 없어도 실행됨
    
    [path variable]
    - URL path variable을 Controller에서 @PathVariable 이용하여 받아오기
    ```
    @GetMapping(url주소/{value})
    public String 함수이름(@PathVariable String value) {
        return 템플릿파일명;
    }
    ```
    - 메서드의 매개변수 value에 URL path variable로 입력된 값이 전달됨
    - 만약, path variable을 다르게 설정하고 싶다면?
    ```
    GetMapping(url주소/{value}/{value2})
    public String 함수이름(@PathVariable String value, @PathVariable("value2") String abc) {
        return 템플릿파일명;
    }
    ```
    - @PathVariable의 괄호 안에 경로변수를 작성하고 원하는 메서드의 매개변수명 사용
      ㄴ 위 예제의 경우 value2 경로 변수의 값이 abc라는 변수에 저장
      ㄴ ex. GET "URL주소/1/2" 라는 요청 시, abc 변수에 2가 저장됨

--- Spring/spring-boot-default/src/main/java/codingon/spring_boot_default/controller/_02_restapi/RestController.java
+++ Spring/spring-boot-default/src/main/java/codingon/spring_boot_default/controller/_02_restapi/RestController.java
@@ -1,7 +1,10 @@
 package codingon.spring_boot_default.controller._02_restapi;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RestController {
@@ -10,4 +13,84 @@ public class RestController {
     @GetMapping("/")
     public String getReq() { return "_02_restapi/req"; }
 
+//    GET 요청
+
+//    @RequestParam 어노테이션
+//    - HTTP 요청 파라미터를 메서드 매개변수에 바인딩
+//    - query string중에서 name key에 대한 value를 String name에 매핑
+//    - required=true 가 기본값(생략됨)이므로 요청 URL에서 name key, age key를 필수로 보내야 함
+//    - 퀴즈) age키 값을 보내지 않으면 에러가 발생하지만, name키 값은 보내지 않아도 잘 동작함? 왜 에러가 발생하지 않을까?
+//        => int는 primitive type이지만, String은 빈 문자열도 유효한 값으로 취급되기떄문
+
+//    #1 : http://localhost:8080/get/res1?name=김도이&age=27
+    @GetMapping("get/res1")
+    public String getRes1(@RequestParam(value = "name") String name,
+                          @RequestParam(value = "age") int age,
+                          Model model) {
+        System.out.println("name = "+name); // name = 김도이
+        System.out.println("age = "+age);   // age = 27
+
+//        view에 전달할 데이터를 Model 객체에 추가
+//        return될 res.html 에 전송될 속성 name과 age에 값 전달
+        model.addAttribute("name",name);
+        model.addAttribute("age",age);
+
+//        응답 결과를 보여줄 뷰(resources/templates/.html) 이름 반환
+        return "_02_restapi/res";
+    }
+
+//    #2 : http://localhost:8080/get/res2?
+    @GetMapping("/get/res2")
+    public String getRes2(@RequestParam(value="name", required=false) String name,
+                          Model model) {
+        System.out.println("name = "+name); // 처음에 name = null 출력
+
+        model.addAttribute("name",name);
+
+        return "_02_restapi/res";
+    }
+
+//    @PathVariable 어노테이션
+//    - URL path variable을 사용할 때 필요한 어노테이션
+//    - 기본적으로 path variable은 값을 가져야 함 (즉, 값이 없으면 404 error)
+
+//    참고. URL의 path variable과 해당 메서드의 매개변수명을 다르게 사용하고 싶다면?
+//    ex. @Path variable(value="param2") int age
+
+//    #3 : http://localhost:8080/get/res3/홍길동/20, /get/res3/홍길동(error)
+    @GetMapping("/get/res3/{param1}/{param2}")
+    public String getRes3(@PathVariable String param1,
+                          @PathVariable(value="param2") int age,
+                          Model model) {
+        System.out.println("name = "+param1);   // 홍길동
+        System.out.println("age = "+age);       // 20
+
+        model.addAttribute("name",param1);
+        model.addAttribute("age",age);
+
+        return "_02_restapi/res";
+    }
+
+//    #4
+//    선택적으로 받아오는 path variable이 있으면, {} 안에 경로 여러개 설정
+//    path variable 중에서 name은 필수경로변수, age는 선택경로변수 라면?
+//    - required = false 옵션을 선택경로변수에서는 사용해야 하고,
+//    - optional 한 변수가 맨 뒤에 와야함(선택경로변수가)
+
+//    참고. 선택경로변수 age의 타입이 int가 아니라 Integer인 이유?
+//    - age (정수형) optional한 값이므로 null이 가능
+//    - primitive type (int) 은 null 값을 가질 수 없음
+//    - 따라서, reference type인 객체를 사용하고자 Integer 선언 (null 값이 들어올 수 있도록)
+    @GetMapping({"get/res4/{name}/{age}", "/get/res4/{name}"})
+    public String getRes4(@PathVariable String name,    // default required=true로 name 경로변수 설정
+                          @PathVariable(required = false) Integer age,   // 선택적으로 받는 경로변수(뒤에 와야함)
+                          Model model) {
+        System.out.println("name = "+name);
+        System.out.println("age = "+age);
+
+        model.addAttribute("name",name);
+        model.addAttribute("age",age);
+        
+        return "_02_restapi/res";
+    }
 }
