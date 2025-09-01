a6b5a32a93122708df339e50ca51a54b1e0d9b0f
Doi Kim <doyclub@naver.com>
2025-09-01 16:38:33 +0900

    250901월_SpringBoot(5-4) 컨트롤러, 서비스 작성

--- Spring/spring-boot-mybatis/src/main/java/codingon/spring_boot_mybatis/controller/UserController.java
+++ Spring/spring-boot-mybatis/src/main/java/codingon/spring_boot_mybatis/controller/UserController.java
@@ -1,5 +1,26 @@
 package codingon.spring_boot_mybatis.controller;
 
-public class UserController {
+import codingon.spring_boot_mybatis.dto.UserDTO;
+import codingon.spring_boot_mybatis.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController // RESTful 웹 서비스의 컨트롤러
+// - 해당 클래스 "모든 메서드"의 반환 값이 자동으로 JSON 형식으로 변환되어 HTTP 응답 본문에 포함
+// - @Controller + @ResponseBody 를 결합한 어노테이션
+@RequestMapping("/api/users") // 해당 클래스의 기본 요청 경로를 지정
+public class UserController {
+//    Controller 게층은 Service 게층을 사용 -> 의존
+    @Autowired
+    private UserService userService;
+
+//    모든 사용자 정보를 리스트로 반환
+    @GetMapping
+    public List<UserDTO> listUsers() {
+        return userService.getAllUsers();
+    }
 }
--- Spring/spring-boot-mybatis/src/main/java/codingon/spring_boot_mybatis/service/UserService.java
+++ Spring/spring-boot-mybatis/src/main/java/codingon/spring_boot_mybatis/service/UserService.java
@@ -1,5 +1,52 @@
 package codingon.spring_boot_mybatis.service;
 
-public class UserService {
+import codingon.spring_boot_mybatis.domain.User;
+import codingon.spring_boot_mybatis.dto.UserDTO;
+import codingon.spring_boot_mybatis.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service    // 이 클래스가 서비스 계층의 컴포넌트임을 나타냄
+public class UserService {
+//    UserService 가 UserMapper 를 사용하기에 UserMapper 인터페이스의 구현체를 자동 주입받음
+    @Autowired  // 자동주입 받는 어노테이션
+    private UserMapper userMapper;
+    
+//    모든 사용자의 정보(DB로 부터 읽어온 domain.User 리스트)를 UserDTO 로 반환
+    public List<UserDTO> getAllUsers() {
+//        1) 모든 domain.User 객체 가져옴
+        List<User> users = userMapper.findAll();
+
+//        2) 새로운 DTO 객체 생성
+        List<UserDTO> userDTOs = new ArrayList<>();
+
+//        3) 반복문을 이용해 각 user 객체를 userDTO로 변환하고 리스트에 추가
+        for (User user: users) {
+            UserDTO userDTO = convertToDto(user);
+            userDTOs.add(userDTO);
+        }
+//        4) UserDTO 리스트 반환
+        return userDTOs;
+    }
+
+//    domain to dto
+    private UserDTO convertToDto(User user) {
+        UserDTO dto = new UserDTO();
+        dto.setId(user.getId());
+        dto.setEmail(user.getEmail());
+        dto.setUsername(user.getUsername());
+        dto.setNo((int) (user.getId() + 100));
+
+        return dto;
+    }
+    
+//    참고. domain.User 와 dto.UserDTO 를 서로 변환하는 이유
+//    - domain.User : 데이터베이스 엔티티를 표현
+//    - dto.UserDTO : 클라이언트와 데이터 전송에 사용
+//    DTO를 사용하게 되면 클라이언트의 요구사항에 맞춰서 데이터 구조를 쉽게 변경
+//    -> 필요한 데이터만 클라이언트에 전송해서 네트워크 부하도 줄일 수 있다
+//    -> API 버전 관리 용이 (엔티티(도메인) 변경 시 DTO를 통해 이전 버전과의 호환성 유지 가능)
 }
