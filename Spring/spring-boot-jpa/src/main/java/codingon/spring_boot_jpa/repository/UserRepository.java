0b14dff1b6352580537a8a2cfcf61ea8055f6843
d0ikim <doyclub@naver.com>
2025-09-05 17:25:36 +0900

    250905금_SpringBoot(8-3) JPA_유저CRUD_Repository - repository 메서드 사용

--- Spring/spring-boot-jpa/src/main/java/codingon/spring_boot_jpa/repository/UserRepository.java
+++ Spring/spring-boot-jpa/src/main/java/codingon/spring_boot_jpa/repository/UserRepository.java
@@ -2,11 +2,29 @@ package codingon.spring_boot_jpa.repository;
 
 import codingon.spring_boot_jpa.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 //    기본적인 CRUD 작업을 위한 메서드들을 제공 받음
 //    ㄴ findAll(), findById(), save(), deleteById() 등
 //    첫번째 제네릭 타입은 관련 테이블, 두번째 제네릭 타입은 관련 테이블 PK의 타입
 
+    /*
+//    case1. repository 메서드 사용
+//    1. 사용자 이름으로 n명 조회
+//    => select * from user where username = ?
+    List<User> findByUsername(String username);
 
+//    2. 검색어 보냈을 때 사용자이름/이메일에 특정 문자열이 포함된 모든 사용자 찾기
+//    => select * from user where username like %xx% or email like %xx%
+    List<User> findByUsernameContainingOrEmailContaining(String name, String email);
+
+//    3. 이름이 존재하는지
+    boolean existsByUsername(String username);
+    */
+    
+//    case2. @Query 어노테이션 사용
 }
--- Spring/spring-boot-jpa/src/main/java/codingon/spring_boot_jpa/service/UserService.java
+++ Spring/spring-boot-jpa/src/main/java/codingon/spring_boot_jpa/service/UserService.java
@@ -30,6 +30,66 @@ public class UserService {
 
         return userDTOs;
     }
+    
+//    특정 id의 사용자 정보를 반환
+    public UserDTO getUserById(Long id) {
+//        JPA가 기본 제공하는 findById()로 특정 유저를 찾을 텐데
+//        있다면 User객체로 반환, 찾지 못하면 null 반환
+        User user = userRepository.findById(id).orElse(null);
+
+        if (user == null) {
+            throw new RuntimeException("User not found");
+        }
+
+        return convertToDTO(user);
+    }
+
+//    새 사용자 생성
+    public void createUser(UserDTO userDTO) {
+//        1. 클라이언트한테 추가해야할 정보를 받음
+        User user = convertToEntity(userDTO);
+
+//        2. repository 에게 추가 요청
+        userRepository.save(user);
+    }
+
+//    특정 id의 사용자 정보를 수정
+    public UserDTO updateUser(Long id, UserDTO userDTO) {
+        User user = convertToEntityWithId(id, userDTO);
+        userRepository.save(user);
+        return convertToDTO(user);
+    }
+
+//    특정 id의 사용자 삭제
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    /// ////////////////////////////////////////////////
+    public List<UserDTO> getUserByUsername(String username) {
+        List<User> users = userRepository.findByUsername(username);
+        List<UserDTO> userDTOs = new ArrayList<>();
+        for (User user:users) {
+            userDTOs.add(convertToDTO(user));
+        }
+        return userDTOs;
+    }
+
+    public List<UserDTO> searchUsers(String keyword) {
+        List<User> users = userRepository.findByUsernameContainingOrEmailContaining(keyword, keyword);
+//        - 첫 번째 인자: username 을 검색하기 위한 keyword 매개값
+//        - 두 번째 인자: email 을 검색하기 위한 keyword 매개값
+        List<UserDTO> userDTOs = new ArrayList<>();
+        for (User user:users) {
+            userDTOs.add(convertToDTO(user));
+        }
+
+        return userDTOs;
+    }
+
+    public boolean isUsernameExists(String username) {
+        return userRepository.existsByUsername(username);
+    }
 
 //    entity(domain) to dto
     private UserDTO convertToDTO(User user) {
@@ -39,6 +99,24 @@ public class UserService {
                 .email(user.getEmail())
                 .username(user.getUsername())
                 .id(user.getId())
+                .build();
+    }
+
+//    dto to entity(domain)
+    private User convertToEntity(UserDTO dto) {
+        return User.builder()
+                .id(dto.getId())
+                .username(dto.getUsername())
+                .email(dto.getEmail())
+                .build();
+    }
+
+//    dto to entity with id
+    private User convertToEntityWithId(Long id, UserDTO dto) {
+        return User.builder()
+                .id(id)
+                .username(dto.getUsername())
+                .email(dto.getEmail())
                 .build();
     }
 }
